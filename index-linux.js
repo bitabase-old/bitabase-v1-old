bb565b683504fd69610a26fdfd4b8374d6e87819
Mark Wylde <me@markwylde.com>
2020-05-25 22:38:57 +1000

    Another attempt at cross compilation

--- index-linux.js
+++ index-linux.js
@@ -1,15 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-const binPaths = [
-  path.join(__dirname, './node_modules/sqlite3/lib/binding/node-v72-linux-x64/node_sqlite3.node'),
-  path.join(__dirname, './dist/rqlite-linux/rqlited')
-]
-binPaths.forEach(binPath => {
-  fs.createReadStream(binPath)
-})
+fs.createReadStream(path.join(__dirname, './node_modules/sqlite3/lib/binding/node-v72-linux-x64/node_sqlite3.node'))
+
+const fileBuffer = fs.readFileSync(path.join(__dirname, './dist/rqlite-linux/rqlited'))
+const rqlitePath = '/tmp/bitabase-rqlited'
+fs.writeFileSync(rqlitePath, fileBuffer)
+fs.chmodSync(rqlitePath, '755');
 
 const {start} = require('./start')
 start({
-  rqliteBinPath: path.join(__dirname, './dist/rqlite-linux/rqlited')
+  rqliteBinPath: rqlitePath
 })
--- index-win.js
+++ index-win.js
@@ -1,15 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-const binPaths = [
-  path.join(__dirname, './node_modules/sqlite3/lib/binding/node-v72-win32-x64/node_sqlite3.node'),
-  path.join(__dirname, './dist/rqlite-win/rqlited.exe')
-]
-binPaths.forEach(binPath => {
-  fs.createReadStream(binPath)
-})
+fs.createReadStream(path.join(__dirname, './node_modules/sqlite3/lib/binding/node-v72-win32-x64/node_sqlite3.node'))
+
+const fileBuffer = fs.readFileSync(path.join(__dirname, './dist/rqlite-win/rqlited.exe'))
+const rqlitePath = 'bitabase-rqlited.exe'
+fs.writeFileSync(rqlitePath, fileBuffer)
+fs.chmodSync(rqlitePath, '755');
 
 const {start} = require('./start')
 start({
-  rqliteBinPath: path.join(__dirname, './dist/rqlite-win/rqlited.exe')
+  rqliteBinPath: rqlitePath
 })
