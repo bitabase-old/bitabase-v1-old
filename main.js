936e0a5e1bbd5ec812d419e38d09f8c9d4a8363f
Mark Wylde <me@markwylde.com>
2020-06-07 08:01:28 +1000

    Split CLI into commands; Exit code on error;

--- main.js
+++ main.js
@@ -9,34 +9,50 @@ const minimist = require('minimist');
 const packageJson = require('./package.json');
 const args = minimist(process.argv);
 
-function main (rqlitePath) {
-  if (args.help) {
-    console.log(`
-${chalk.green(chalk.bold('📦 Bitabase'))} ${chalk.green(`- v${packageJson.version}`)}
+function showHelp () {
+  console.log(`
+  ${chalk.green(chalk.bold('📦 Bitabase'))} ${chalk.green(`- v${packageJson.version}`)}
 The scalable, sharded database engine.
 https://docs.bitabase.com
 
-The following arguments are available when starting Bitabase
+The following commands and arguments are available when starting Bitabase
 
-Arguments:
-  --rqlite-http-bind=host:port   Host and port to bind the rqlite node to (default: 0.0.0.0:4001)
-  --rqlite-raft-bind=host:port   Host and port to bind the rqlite node to (default: 0.0.0.0:4002)
-  --rqlite-join=host:port        Join an rqlite cluster (default: none, eg: http://0.0.0.0:4001)
-  --rqlite-storage=/path         Where to store rqlite transaction log (default: /tmp/rqlite-bitabase)
+Commands:
+  start                            Start the bitabase server stack
 
-  --manager-bind=host:port       Host and port to bind the manager to (default: 0.0.0.0:8081)
-  --gateway-bind=host:port       Host and port to bind the gateway to (default: 0.0.0.0:8082)
-  --server-bind=host:port        Host and port to bind the server to (default: 0.0.0.0:8000)
+    --rqlite-http-bind=host:port   Host and port to bind the rqlite node to (default: 0.0.0.0:4001)
+    --rqlite-raft-bind=host:port   Host and port to bind the rqlite node to (default: 0.0.0.0:4002)
+    --rqlite-join=host:port        Join an rqlite cluster (default: none, eg: http://0.0.0.0:4001)
+    --rqlite-storage=/path         Where to store rqlite transaction log (default: /tmp/rqlite-bitabase)
 
-`.trim() + '\n');
+    --manager-bind=host:port       Host and port to bind the manager to (default: 0.0.0.0:8081)
+    --gateway-bind=host:port       Host and port to bind the gateway to (default: 0.0.0.0:8082)
+    --server-bind=host:port        Host and port to bind the server to (default: 0.0.0.0:8000)
+
+  `.trim() + '\n');
+}
+
+function main (rqlitePath) {
+  if (args.help || args._.length === 2) {
+    showHelp();
+    console.log(chalk.red('No command specified'));
+    process.exit(1);
     return;
   }
 
-  const { start } = require('./start');
-  start({
-    ...args,
-    rqliteBinPath: rqlitePath
-  });
+  if (args._[2] === 'start') {
+    const { start } = require('./start');
+    start({
+      ...args,
+      rqliteBinPath: rqlitePath
+    });
+    return;
+  }
+
+  showHelp();
+  console.log(args);
+  console.log(chalk.red(`Unknown command "${args._[2]}"`));
+  process.exit(1);
 }
 
 module.exports = main;
