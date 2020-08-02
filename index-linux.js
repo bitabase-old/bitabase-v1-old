f1d18e254636e94a572634aa1ef573d4e9ad6867
Mark Wylde <me@markwylde.com>
2020-08-02 15:26:23 +1000

    Update sqlite.node path for linux and windows

--- index-linux.js
+++ index-linux.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const main = require('./main');
 
-fs.createReadStream(path.join(__dirname, './node_modules/sqlite3/lib/binding/node-v72-linux-x64/node_sqlite3.node'));
+fs.createReadStream(path.join(__dirname, './node_modules/sqlite3/lib/binding/napi-v3-linux-x64/node_sqlite3.node'));
 
 const fileBuffer = fs.readFileSync(path.join(__dirname, './dist/rqlite-linux/rqlited'));
 const rqlitePath = '/tmp/bitabase-rqlited';
--- index-win.js
+++ index-win.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const main = require('./main');
 
-fs.createReadStream(path.join(__dirname, './node_modules/sqlite3/lib/binding/node-v72-win32-x64/node_sqlite3.node'));
+fs.createReadStream(path.join(__dirname, './node_modules/sqlite3/lib/binding/napi-v3-win32-x64/node_sqlite3.node'));
 
 const fileBuffer = fs.readFileSync(path.join(__dirname, './dist/rqlite-win/rqlited.exe'));
 const rqlitePath = 'bitabase-rqlited.exe';
